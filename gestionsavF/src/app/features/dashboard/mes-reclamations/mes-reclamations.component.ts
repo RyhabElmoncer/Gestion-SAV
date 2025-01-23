42ddd0cd6fbf4d36cb7c0a8f3bb36b7aa39a8421
ryhab <ryhabelmoncerr@gmail.com>
2025-01-23 21:42:10 +0100

    ajout article with cin

--- gestionsavF/src/app/features/dashboard/mes-reclamations/mes-reclamations.component.ts
+++ gestionsavF/src/app/features/dashboard/mes-reclamations/mes-reclamations.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReclamationService } from '../../../core/services/Reclamation/reclamation.service';
 import { Reclamation } from '../../../models/Reclamation';
 
 @Component({
   selector: 'app-mes-reclamations',
   templateUrl: './mes-reclamations.component.html',
-  styleUrl: './mes-reclamations.component.css'
+  styleUrls: ['./mes-reclamations.component.css'],
 })
-export class MesReclamationsComponent { reclamations: Reclamation[] = [];
+export class MesReclamationsComponent implements OnInit {
+  reclamations: Reclamation[] = [];
 
   constructor(private reclamationService: ReclamationService) {}
 
   ngOnInit(): void {
-    this.reclamationService.obtenirToutesReclamations().subscribe(
-      (data) => {
-        this.reclamations = data;
-      },
-      (error) => {
-        console.error('Erreur lors de la récupération des réclamations', error);
-      }
-    );
+    this.chargerReclamations();
+  }
+
+  private chargerReclamations(): void {
+    this.reclamationService.obtenirToutesReclamations().subscribe({
+      next: (data) => (this.reclamations = data),
+      error: (err) => console.error('Erreur lors de la récupération des réclamations :', err),
+    });
   }
 }
